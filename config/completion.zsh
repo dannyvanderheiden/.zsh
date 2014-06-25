13c6103260ffa0e20d12c33c28ac19d5c0d44aad
Danny van der Heiden <dannyvdheiden@gmail.com>
2014-06-25 22:09:09 +0200

    Enable advanced tab-completion.

---
+++ config/completion.zsh
@@ -0,0 +1,24 @@
+# Enable advanced Tab-completion
+autoload -Uz compinit
+compinit
+
+# Make adding flags or prefixing arguments easier
+after-first-word() {
+  zle beginning-of-line
+  zle forward-word
+}
+zle -N after-first-word
+bindkey "^X1" after-first-word
+
+# Define completion
+zstyle ':completion:*' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=00;31'
+zstyle -e ':completion:*:approximate:*' max-errors 'reply=( $(( ($#PREFIX+$#SUFFIX)/2 )) numeric )'
+zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
+
+# Complete with words in history (like Emacs dabbrev) with M-/, M-,
+zstyle ':completion:history-words:*' list no
+zstyle ':completion:history-words:*' menu yes
+zstyle ':completion:history-words:*' remove-all-dups yes
+bindkey "\e/" _history-complete-older
+bindkey "\e," _history-complete-newer
