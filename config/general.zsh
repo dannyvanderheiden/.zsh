bae5a4031496a4683f5cf1589b535d56404ee7f0
Danny van der Heiden <dannyvdheiden@gmail.com>
2014-06-25 21:55:02 +0200

    Create project aliases by defining project root directories.

--- config/general.zsh
+++ config/general.zsh
@@ -19,3 +19,16 @@ alias t="tmux -u"
 
 # Colors in less
 alias less='less -R'
+
+# Create project aliases by defining project root directories
+for PARENT_DIR in ${PROJECT_PARENT_DIRS[@]} ; do
+  if [ -d "$PARENT_DIR" ]; then
+    for PROJECT_DIR in $(/bin/ls $PARENT_DIR); do
+      if [[ ! -z `which $PROJECT_DIR 2> /dev/null` ]]; then
+        if [ -d "$PARENT_DIR/$PROJECT_DIR" ]; then
+          alias "$PROJECT_DIR"="cd $PARENT_DIR/$PROJECT_DIR"
+        fi
+      fi
+    done
+  fi
+done
