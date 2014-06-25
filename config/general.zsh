1f96233866500944ce2d1142492b9936ec2e725d
Danny van der Heiden <dannyvdheiden@gmail.com>
2014-06-25 21:54:21 +0200

    Add some general aliases, like listing files.

---
+++ config/general.zsh
@@ -0,0 +1,21 @@
+# Aliases
+##########
+
+# Listing files
+alias l='ls -halo'
+alias ltr='ls -lt'
+alias lth='l -t|head'
+alias lh='ls -Shl | less'
+alias tf='tail -f -n 100'
+
+# VIM
+alias m='mvim --remote-silent'
+
+# Directories
+alias downloads="cd $HOME/Downloads"
+
+# Tmux in utf-8 mode
+alias t="tmux -u"
+
+# Colors in less
+alias less='less -R'
