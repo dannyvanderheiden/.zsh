f4196db45fb36da93b3d907287a098a8d71154e8
Danny van der Heiden <dannyvdheiden@gmail.com>
2014-06-25 21:51:19 +0200

    Let’s add some colors.

---
+++ config/colors.zsh
@@ -0,0 +1,17 @@
+# Colors, mainly used in prompt
+export reset=$'%{\e[0;00m%}'
+export gray=$'%{\e[0;90m%}'
+export red=$'%{\e[0;31m%}'
+export green=$'%{\e[0;32m%}'
+export yellow=$'%{\e[0;33m%}'
+export blue=$'%{\e[0;34m%}'
+export magenta=$'%{\e[0;35m%}'
+export cyan=$'%{\e[0;36m%}'
+export white=$'%{\e[0;37m%}'
+
+
+# Enable colors
+export CLICOLOR=1
+
+# Color completion
+export LS_COLORS="no=00:fi=00:di=00;34:ln=00;36:pi=40;33:so=00;35:bd=40;33;01:cd=40;33;01:or=01;05;37;41:mi=01;05;37;41:ex=00;35:*.rb=00;31"
