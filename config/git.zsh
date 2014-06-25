d4e90c0b3a32884ec48bb8095cbcb79a565976df
Danny van der Heiden <dannyvdheiden@gmail.com>
2014-06-25 22:13:26 +0200

    Add Git aliases.

---
+++ config/git.zsh
@@ -0,0 +1,35 @@
+# Aliases
+#########
+
+alias cdb='base=$(git rev-parse --show-cdup) && cd $base'
+
+alias upstash='git stash && git pull --ff-only && git stash pop'
+
+alias push='git push'
+
+alias st='git status'
+alias status='git status'
+
+alias co='git checkout'
+alias checkout='git checkout'
+
+alias ci='git commit'
+alias commit='git commit'
+
+alias up='git pull --ff-only'
+
+alias br='git branch'
+alias branch='git branch'
+
+alias lg='git log -p'
+alias ll='git l'
+alias la='git la'
+
+alias aa='git add --all'
+
+alias d='git diff'
+alias df='git diff'
+alias dc='git diff --cached'
+
+alias f='git fetch'
+alias fetch='git fetch'
